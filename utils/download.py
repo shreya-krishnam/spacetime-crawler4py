c0e23ae47552b7b13bec2dc1a46f0eb91afa8224
Shreya Krishnam <50132995+shreya-krishnam@users.noreply.github.com>
2020-11-02 19:43:31 -0800

    Add files via upload

--- utils/download.py
+++ utils/download.py
@@ -5,12 +5,16 @@ import time
 from utils.response import Response
 
 def download(url, config, logger=None):
+    print(url)
     host, port = config.cache_server
     resp = requests.get(
         f"http://{host}:{port}/",
         params=[("q", f"{url}"), ("u", f"{config.user_agent}")])
     if resp:
-        return Response(cbor.loads(resp.content))
+       try:
+          return Response(cbor.loads(resp.content))
+       except EOFError:
+          logger.error(f"EOF error {resp} with url {url}.")
     logger.error(f"Spacetime Response error {resp} with url {url}.")
     return Response({
         "error": f"Spacetime Response error {resp} with url {url}.",
